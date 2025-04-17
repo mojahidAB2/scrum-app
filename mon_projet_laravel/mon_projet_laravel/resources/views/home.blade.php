de73dda9faca2d9164e10e21b7c53d3f66f4efc3
Oumaimaelammari1234 <elammarioumaima46@gmail.com>
2025-04-17 12:32:44 +0100

    Update home.blade.php

--- mon_projet_laravel/mon_projet_laravel/resources/views/home.blade.php
+++ mon_projet_laravel/mon_projet_laravel/resources/views/home.blade.php
@@ -7,6 +7,7 @@
 <body style="text-align: center; margin-top: 100px;background-color:rgb(212, 50, 4); color: white;">
  
     <p>Tu as réussi à remplacer la page par défaut !</p>
+     <p>Tu as réussi à remplacer la page par défaut !</p>
     <p>cette view est afficher le code html</p>
 </body>
 </html>
